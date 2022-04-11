72d100e02478101c77f8adbf77e157785aa77d41
jsuisjuan <juanc.s.delima@gmail.com>
2022-04-10 21:32:01 -0300

    organize repository

---
+++ Data Structure/class-aninhada.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    class B {
+    private:
+        int var;
+    public:
+        B (int var) {
+            this->var = var;
+        }
+        int getVar() {
+            return var;
+        }
+    };
+};
+
+int main() {
+    A::B obj(10);
+    cout << obj.getVar() << endl;
+    return 0;
+}
