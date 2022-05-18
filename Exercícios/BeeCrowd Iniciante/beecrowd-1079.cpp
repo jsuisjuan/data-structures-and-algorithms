5ef9d0b5ff8e4396ed72f858eb2c4a0cee25f955
jsuisjuan <juanc.s.delima@gmail.com>
2022-05-18 14:36:12 -0300

    add readme

---
+++ Exercícios/BeeCrowd Iniciante/beecrowd-1079.cpp
@@ -0,0 +1,10 @@
+// beecrowd | 1079: Médias Ponderadas
+// https://www.beecrowd.com.br/judge/pt/problems/view/1079
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
